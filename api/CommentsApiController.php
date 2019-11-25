77e0d99aeaf66ed80937ec0a2e35e24eacac93ed
PicassoOctavio <octaviopicasso.ma@gmail.com>
2019-11-25 18:33:43 -0300

    funciones de api controller hechas menos update

--- api/CommentsApiController.php
+++ api/CommentsApiController.php
@@ -15,6 +15,43 @@ class CommentsApiController{
     private function getData() {
         return json_decode($this->data);
     }
+    public function getComments($params = null){
+        $comments = $this ->model ->getAllComments();
+        $this->view->response($comments, 200);
+    }
+    public function getComment($params = null){
+        $idComment = $params['ID'];
+        $comment = $this ->model ->getComment($idComment);
+        if($comment){
+            $this ->view ->response($comment, 200);
+        } else{
+            $this ->view ->response("Comentario no encontrado", 404);
+        }
+    }
+    public funtion deleteComment($params = null){
+        $id = $params[':ID'];
+        $comment = $this ->model -> getComment($id);
+        if($comment){
+            $this ->model ->delete($id);
+            $this ->view ->response ("La tarea fue borrada con exito", 200);
+        } else{
+            $this ->view ->response ("La tarea con el id={$id} no existe", 404);
+        }
+        //OBTENER COMENTARIO
+        //si el comentario existe-> model-> borrar;
+        //sino, tarea no encontrada 
+    }
+    public function addComment($params = null){
+            $data = $this ->getData();
+            $idComment = $this ->model ->save($data ->description);
+            $comment = $this ->model ->getComment($idComment);
+            if($comment){
+                $this ->view ->response($comment, 200);
+            } else{
+                $this ->view ->response("El comentario no fue creado", 500);
+            }
+    }
 
 
+    
 }
--- model/CommentsModel.php
+++ model/CommentsModel.php
@@ -7,15 +7,30 @@ class CommentsModel{
         $this->db = new PDO('mysql:host=localhost;'.'dbname=db_tpespecial;charset=utf8', 'root', '');
     }
 
-    public function getComments(){//obtengo TODOS los comentarios
+    public function getAllComments(){//obtengo TODOS los comentarios
         $query = $this ->db -> prepare("SELECT * FROM comentarios");
         $ok = $query-> execute();
         if(!$ok){
             var_dump($query->errorInfo());
             die();
-        }
+        }   
         $comments = $query->fetchAll(PDO::FETCH_OBJ);
         return $comments;
+    }
+    public function getComment ($params = null){
+        $query = $this ->db ->prepare("SELECT*FROM comentario WHERE id_comment = ?");
+        $ok = $query ->execute(array($idComment));
+        if(!$ok){
+            var_dump($query->errorInfo());
+            die();
+        }
+        $comment = $query ->fetch(PDO::FETCH_OBJ);
+        return $comment; 
+    } 
+    public function saveComment($description){
+        $query = $this ->db ->prepare("INSERT INTO comentarios(text) VALUES(?)");
+        $query = execute(array($description));
+        return $this ->db ->lastInsertId();
     }
 
 }
